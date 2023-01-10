6b4972099d4fb679a3ade3fde86e7d6b5a573c3e
TuPhung <tu.phung@tuni.fi>
2023-01-10 21:21:30 +0200

    Add prettier to the config

--- src/App.js
+++ src/App.js
@@ -10,17 +10,12 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
     </div>
   );
-};
+}
 
 export default App;
