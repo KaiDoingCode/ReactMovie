6f428ec67b002300dc5cf1e92c7746cd3fcf3332
TuPhung <tu.phung@tuni.fi>
2023-01-17 15:36:19 +0200

    Add front end component, Header, Navbar, Homepage & Slider

--- src/App.js
+++ src/App.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import { Provider } from 'react-redux';
+import React, { useEffect } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-import './App.css';
-import store from './redux/store';
+import './App.scss';
+import Header from './components/header/Header';
+import Main from './components/main/Main';
+import Details from './components/content/details/Details';
+import ErrorBoundary from './components/error/ErrorBoundary';
+import { appRoutes } from './redux/actions/routes';
+import { AppRoutes } from './routes';
 
-const App = () => {
+const App = (props) => {
+  const { appRoutes } = props;
+  const routesArray = [
+    {
+      id: 1,
+      path: '/',
+      component: Main
+    },
+    {
+      id: 2,
+      path: '/:id/:name/details',
+      component: Details
+    }
+  ];
+
+  useEffect(() => {
+    appRoutes(routesArray);
+  }, [routesArray, appRoutes]);
+
   return (
-    <Provider store={store}>
-      <div className="app">
-        <h1>Setup React Redux</h1>
-      </div>
-    </Provider>
+    <div className="app">
+      <BrowserRouter>
+        <ErrorBoundary>
+          <Header />
+        </ErrorBoundary>
+        <AppRoutes />
+      </BrowserRouter>
+    </div>
   );
 };
 
-export default App;
+App.propTypes = {
+  appRoutes: PropTypes.func
+};
+
+export default connect(null, { appRoutes })(App);
